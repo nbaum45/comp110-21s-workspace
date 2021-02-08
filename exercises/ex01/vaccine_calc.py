c6e6179260ed96d925714ede18fdec592447854c
Nicole Baum <nbaum@live.unc.edu>
2021-02-08 00:26:02 -0500

    Ex01

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -18,3 +18,19 @@ from datetime import timedelta
 
 
 # Begin your solution here...
+population_size: int = int(input("Population: "))
+doses_so_far: int = int(input("Doses administered: "))
+dose_rate: int = int(input("Doses per day: "))
+target_percent: int = int(input("Target percent vaccinated: "))
+
+current_percent_of_doses: float = (doses_so_far / 2) / population_size
+percent_doses_still_needed: float = (target_percent / 100) - current_percent_of_doses
+number_of_people_who_need_doses: float = percent_doses_still_needed * population_size
+number_of_doses_needed: float = number_of_people_who_need_doses * 2
+days_needed: int = round(number_of_doses_needed / dose_rate)
+
+days_needed_timedelta: timedelta = timedelta(days_needed)
+today: datetime = datetime.today()
+target_date: datetime = today + days_needed_timedelta
+
+print("We will reach " + str(target_percent) + "% " + "vaccination in " + str(days_needed) + " days, which falls on " + target_date.strftime("%B %d, %Y") + ".")  
