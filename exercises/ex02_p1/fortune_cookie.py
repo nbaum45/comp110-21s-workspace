975c0ecb3e5db8c2293dddbcb467fda1032fe65c
Nicole Baum <nbaum@live.unc.edu>
2021-02-11 23:09:00 -0500

    Exercise 1 Part 1 progress

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -2,17 +2,28 @@
 
 from random import randint
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730287108"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    # TODO 2: Print the result of calling your fortune_cookie function.
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
 
 
-# TODO 1: Define your fortune_cookie function here.
+def fortune_cookie() -> str:
+    fortune: int = randint(1, 4)
+    if fortune < 3:
+        if fortune < 2:
+            return "You will adopt a long-haired cat named Tom."
+        else:
+            return "You will meet the love of your life this year."
+    else:
+        if fortune < 4:
+            return "You will enjoy a large chocolate chip cookie with icecream soon."
+        else:
+            return "You will read an inspring book this month."
 
 
 # Python Idiom for "starting" the program when run as a module.
--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,15 +1,25 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730287108"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
+    print(tar_heels(choice))
 
 
-# TODO 1: Define the tar_heels function, and its logic, here.
+def tar_heels(user_input: int) -> str:    
+    if user_input % 2 == 0 and user_input % 7 == 0:
+        return "TAR HEELS"
+    else:
+        if user_input % 2 == 0:
+            return "TAR"
+        else: 
+            if user_input % 7 == 0:
+                return "HEELS"
+            else: 
+                return "CAROLINA"
 
 
 if __name__ == "__main__":
